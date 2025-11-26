4328693047d82ece28bfda26457f806e493db0d6
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-11-26 11:47:48 +0330

    [Feat] : exceptions are handled correctly in this file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,50 @@
 <?php
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (\Throwable $e, $request) {
+            if ($request->expectsJson()) {
+                $status = 500;
+                $message = 'Server Error';
+                if ($e instanceof ModelNotFoundException) {
+                    $status = 404;
+                    $message = 'Resource not found';
+                } elseif ($e instanceof AuthorizationException) {
+                    $status = 403;
+                    $message = 'Forbidden';
+                } elseif ($e instanceof ValidationException) {
+                    $status = 422;
+                    return response()->json(
+                        [
+                            'success' => false,
+                            'message' => 'Validation failed',
+                            'errors' => $e->errors()
+                        ],
+                        422
+                    );
+                }
+                return response()->json(
+                    [
+                        'success' => false,
+                        'message' => $message,
+                    ],
+                    $status,
+                );
+            }
+        });
     })->create();
