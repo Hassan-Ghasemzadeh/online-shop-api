091226fdb41848b12004e13b53e4a9912f0c5cd5
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-11-26 16:55:59 +0330

    [Refactor] : order model class completed

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total',
+        'status',
+        'items',
+    ];
+
+    protected $casts = [
+        'items' => 'array',
+        'total' => 'decimal:2'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
 }
