88a97e4cf4f0eda6e4a433401f6b8ca1cc737ea0
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-11-26 16:59:13 +0330

    [Refactor] : payment model class completed

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'provider',
+        'status',
+        'transaction_id',
+        'meta',
+    ];
+
+    protected $casts = [
+        'meta' => 'array'
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
