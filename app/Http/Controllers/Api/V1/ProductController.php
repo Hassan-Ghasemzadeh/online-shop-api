43192f9bf8bea48948cad58268799f255623f4a9
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-11-26 18:16:40 +0330

    [feat] : product controller completed

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -3,47 +3,65 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Services\Contracts\ProductServiceInterface;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    use ApiResponse;
+    protected ProductServiceInterface $productService;
+
+    public function __construct(ProductServiceInterface $productService)
+    {
+        $this->productService = $productService;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $filters = request()->only(['category_id', 'is_active', 'search']);
+        $perPage = request('per_page', 15);
+        $products = $this->productService->list($filters, $perPage);
+        return $this->success($products);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProductRequest $request)
     {
-        //
+        $products = $this->productService->create($request->validated());
+        return $this->success($products, 'Product successfully created', 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $products = $this->productService->get((int)$id);
+        return $this->success($products, 'Request successfully done');
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateProductRequest $request,  $id)
     {
-        //
+        $products = $this->productService->update((int)$id, $request->validated());
+        return $this->success($products);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $this->productService->delete((int)$id);
+        return $this->success(null, 'Successfully deleted', 204);
     }
 }
