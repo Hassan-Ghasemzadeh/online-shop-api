ab558a9f2f2c8b5d5353e0ae21fbf7ff03e84f73
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-11-27 16:31:13 +0330

    [Feat] : api test done

---
+++ tests/Feature/ProductApiTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class ProductApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_authenticated_user_can_create_product(): void
+    {
+        $user = User::factory()->create();
+        $token = JWTAuth::fromUser($user);
+        $category = Category::factory()->create();
+        $response = $this->withHeaders(
+            [
+                'Authorization' => "Bearer {$token}",
+                'Accept' => 'application/json'
+            ]
+        )->postJson('/api/products', [
+            'category_id' => $category->id,
+            'name' => 'New product',
+            'price' => 99.9
+        ]);
+        $response->assertStatus(201)->assertJsonStructure(
+            [
+                'success',
+                'data' => ['id', 'name', 'sku']
+            ]
+        );
+    }
+}
