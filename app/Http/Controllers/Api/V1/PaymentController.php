5ea22524b6d1f398cb6191e8cef6e13c93c8bfa5
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-11-27 12:23:34 +0330

    payment done

--- app/Http/Controllers/Api/V1/PaymentController.php
+++ app/Http/Controllers/Api/V1/PaymentController.php
@@ -3,47 +3,46 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Payment;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Stripe\PaymentIntent;
+use Stripe\Stripe;
+use Throwable;
 
 class PaymentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    use ApiResponse;
+    public function pay(Request $request, int $orderId)
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $request->validate([
+            'payment_method' => 'required|string',
+        ]);
+        $order = Order::findOrFail($orderId);
+        Stripe::setApiKey(config('services.stripe.secret'));
+        DB::beginTransaction();
+        try {
+            $intent = PaymentIntent::create([
+                'amount' => (int) $order->total * 100,
+                'currency' => 'usd',
+                'payment_method' => $request->payment_method,
+                'confirm' => true,
+            ]);
+            $payment = Payment::create([
+                'order_id' => $order->id,
+                'provider' => 'stripe',
+                'status' => 'succeeded',
+                'transaction_id' => $intent->id,
+                'meta' => $intent->toArray(),
+            ]);
+            $order->update(['status' => 'paid']);
+            DB::commit();
+            $this->success($payment);
+        } catch (Throwable $e) {
+            Db::rollBack();
+            return $this->error($e->getMessage());
+        }
     }
 }
