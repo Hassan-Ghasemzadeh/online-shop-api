e111da7502be9b4b3d4129fd57b6fbd7a952ee18
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-11-27 11:07:42 +0330

    [feat] : login and register completed

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -3,9 +3,45 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    use ApiResponse;
+    public function register(Request $request)
+    {
+        $request->validate(
+            [
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:6|confirmed'
+            ]
+        );
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+        return $this->success(['user' => $user, 'token' => $token], 'Successfully registered', 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return $this->error([], 'Invalid credentials', 401);
+            }
+        } catch (JWTException $e) {
+            return $this->error([], 'Could not create token', 500);
+        }
+        return $this->success(['token' => $token]);
+    }
 }
