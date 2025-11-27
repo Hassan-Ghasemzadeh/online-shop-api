c774ef092aba6f8e862ecf1fb871fb7256075fa1
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-11-27 12:08:37 +0330

    [Feat] : order has been completed

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Events\OrderPlaced;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreOrderRequest;
+use App\Models\Order;
+use App\Models\Product;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
+    use ApiResponse;
     /**
      * Display a listing of the resource.
      */
@@ -18,17 +25,43 @@ class OrderController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreOrderRequest $request)
     {
-        //
+        $user = Auth::user();
+        $items = $request->get('items');
+        $total = 0;
+        $snapshot = [];
+        foreach ($items as $item) {
+            $product = Product::findOrFail($item['product_id']);
+            $subtotal =  $product->price * $item['quantity'];
+            $total += $subtotal;
+            $snapshot[] = [
+                'product_id' => $product->id,
+                'name' => $product->name,
+                'price' => $product->price,
+                'quantity' => $item['quantity'],
+                'subtotal' => $subtotal,
+            ];
+        }
+        $order = Order::create(
+            [
+                'user_id' => $user->id,
+                'total' => $total,
+                'status' => 'pending',
+                'items' => $snapshot,
+            ]
+        );
+        event(new OrderPlaced($order));
+        return $this->success($order, 'Order created successfully', 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $order = Order::with('payments')->findOrFail((int)$id);
+        return $this->success($order);
     }
 
     /**
--- app/Http/Requests/StoreOrderRequest.php
+++ app/Http/Requests/StoreOrderRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class StoreOrderRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreOrderRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return Auth::check();
     }
 
     /**
@@ -22,7 +23,9 @@ class StoreOrderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'items' => 'required|array|min:1',
+            'items.*.product_id' => 'required|exists:products,id',
+            'items.*.quantity' => 'required|integer|min:1'
         ];
     }
 }
