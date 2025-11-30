aeb61d1a700d985e00d25b280245f01d4c3a9e14
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-11-30 15:10:04 +0330

    [Feat] : api route created

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\V1\AuthController;
-use App\Http\Controllers\Api\V1\CategoryController;
-use App\Http\Controllers\Api\V1\OrderController;
-use App\Http\Controllers\Api\V1\PaymentController;
-use App\Http\Controllers\Api\V1\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('auth/register', [AuthController::class, 'register']);
-Route::post('auth/login', [AuthController::class, 'login']);
-
-Route::middleware(['auth:api'])->group(function () {
-    Route::apiResource('products', ProductController::class);
-    Route::apiResource('categories', CategoryController::class)->except('create', 'edit');
-    Route::post('orders',[OrderController::class,'store']);
-    Route::get('orders/{id}',[OrderController::class,'show']);
-    Route::post('payments/{order}',[PaymentController::class,'pay']);
+Route::get('/', function () {
+    return view('welcome');
 });
