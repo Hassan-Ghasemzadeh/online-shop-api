34e1eac43b829d3a706bf4a3a0cd3eaaabf79bb6
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-12-16 10:21:52 +0330

    jwt middleware alias added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'auth.jwt' => \App\Http\Middleware\JwtMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->renderable(function (\Throwable $e, $request) {
