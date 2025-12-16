a15041b01d5e98f0af8e6326ada26caa562c85de
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-12-16 15:51:26 +0330

    [Feat] : product service test done

--- tests/Unit/ProductServiceTest.php
+++ tests/Unit/ProductServiceTest.php
@@ -11,10 +11,12 @@ use PHPUnit\Framework\TestCase;
 class ProductServiceTest extends TestCase
 {
     protected $repo;
+    protected $service;
     protected function setUp(): void
     {
         parent::setUp();
         $this->repo = Mockery::mock(ProductRepositoryInterface::class);
+        $this->service = new ProductService($this->repo);
     }
 
     public function test_create_product_generates_sku_if_missing(): void
@@ -22,14 +24,54 @@ class ProductServiceTest extends TestCase
         $this->repo->shouldReceive('create')->once()->andReturnUsing(function ($data) {
             return new Product($data + ['id' => 1]);
         });
-        $service = new ProductService($this->repo);
-        $product = $service->create([
+        $product = $this->service->create([
             'category_id' => 1,
             'name' => 'Test product',
             'price' => 10,
         ]);
         $this->assertNotEmpty($product->sku);
         $this->assertEquals('Test product', $product->name);
+    }
+    public function test_get_product(): void
+    {
+        $mockProduct = new Product([
+            'id' => 1,
+            'category_id' => 1,
+            'name' => 'Test product',
+            'price' => 10,
+        ]);
+        $this->repo->shouldReceive('find')->once()->with(1)->andReturn($mockProduct);
+        $product = $this->service->get(1);
+        $this->assertEquals('Test product', $product->name);
+    }
+    public function test_update_product(): void
+    {
+        $productId = 1;
+        $existingProduct = new Product([
+            'id' => $productId,
+            'name' => 'Old Name',
+            'price' => 10
+        ]);
+
+        $updateData = [
+            'name' => 'Updated Test product',
+            'price' => 1000,
+        ];
+        $this->repo->shouldReceive('find')
+            ->once()
+            ->with($productId)
+            ->andReturn($existingProduct);
+
+        $this->repo->shouldReceive('update')
+            ->once()
+            ->with($existingProduct, $updateData)
+            ->andReturnUsing(function ($product, $data) {
+                $product->fill($data);
+                return $product;
+            });
+        $product = $this->service->update($productId, $updateData);
+        $this->assertEquals(1000, $product->price);
+        $this->assertEquals('Updated Test product', $product->name);
     }
     protected function tearDown(): void
     {
