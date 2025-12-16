d331d8ff1ec1ea2f5438621d11e0bf7d1d00a60d
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-12-16 10:22:27 +0330

    [Feat] : jwt middleware has been completed

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Middleware;
 
+use App\Traits\ApiResponse;
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class JwtMiddleware
 {
+    use ApiResponse;
     /**
      * Handle an incoming request.
      *
@@ -15,6 +21,20 @@ class JwtMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        try {
+            $user = Auth::user();
+            if (!$user) {
+                $this->error([], "User not found", 404);
+            }
+        } catch (Exception $e) {
+            if ($e instanceof TokenInvalidException) {
+                $this->error($e, "Token is invalid", 401);
+            } elseif ($e instanceof TokenExpiredException) {
+                $this->error($e, "Token expired", 401);
+            } else {
+                $this->error($e, "Authorization Token not found", 401);
+            }
+        }
         return $next($request);
     }
 }
