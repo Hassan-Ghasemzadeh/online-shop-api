607d94f116c5aa725eda4ee5fbf4b9d53b4116c8
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-12-16 15:08:37 +0330

    [Refactor] : moved mucking to setUp func

--- tests/Unit/ProductServiceTest.php
+++ tests/Unit/ProductServiceTest.php
@@ -10,14 +10,19 @@ use PHPUnit\Framework\TestCase;
 
 class ProductServiceTest extends TestCase
 {
+    protected $repo;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->repo = Mockery::mock(ProductRepositoryInterface::class);
+    }
 
     public function test_create_product_generates_sku_if_missing(): void
     {
-        $repo = Mockery::mock(ProductRepositoryInterface::class);
-        $repo->shouldReceive('create')->once()->andReturnUsing(function ($data) {
+        $this->repo->shouldReceive('create')->once()->andReturnUsing(function ($data) {
             return new Product($data + ['id' => 1]);
         });
-        $service = new ProductService($repo);
+        $service = new ProductService($this->repo);
         $product = $service->create([
             'category_id' => 1,
             'name' => 'Test product',
