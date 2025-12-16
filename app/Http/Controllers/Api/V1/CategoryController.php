1889c003c943177967d2fc53d2aa5af31654bac4
Hassan-Ghasemzadeh <hghasemzadeh38@gmail.com>
2025-12-16 11:08:03 +0330

    [Feat] : category controller done

--- app/Http/Controllers/Api/V1/CategoryController.php
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -3,24 +3,35 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Services\CategoryService;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+    use ApiResponse;
+    protected CategoryService $categoryService;
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $this->success($this->categoryService->all(), "Successfully got all data");
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCategoryRequest $request)
     {
-        //
+        $request->validate();
+        $category = $this->categoryService->create($request->all());
+        return $this->success($category, "Successfully got the category", 201);
     }
 
     /**
